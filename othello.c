d1c2bcc6c7ff07b36f5a9edd437f9409ba008e2f
JMMatosF <jmatosfernandes@live.com.pt>
2022-07-20 00:06:49 +0100

    hey

--- othello.c
+++ othello.c
@@ -6,12 +6,43 @@
 
 int main(int argc, char** argv){
     char board [SZ][SZ], colunas_ficheiro[64], y, linhas_ficheiro[64];
-    int moves[SZ][SZ], x= 0, no_of_games = 0, n_movimentos = 0, player = 0, file = 0, invalid_moves = 0;
+    int player = 0, file = 0;
 
     init_board(board);
     int end = 0;
     int turn = 0;
     init_board(board);
+    FILE *ficheiro = NULL;  //CONSOANTE O QUE SE ESCREVE NO TERMINAL, ELE JOGA INTERATIVAMENTE OU ATRAVES DE UM FICHEIRO DE TEXTO
+    if(argc < 2){
+        printf("VAI SER MOSTRADO O TABULEIRO INICIAL: \n");
 
+        oponente(player);
+
+        printf("O COMPUTADOR JOGA PRIMEIRO\n");
+
+        input_output(board,turn);  //CHAMADA DAS FUNCOES
+        print_board(board);
+
+    }
+
+    else if(argc == 2){
+        ficheiro = fopen (/*argv[1]*/"jogadas.txt", "r"); //ABERTURA PARA LEITURA DO FICHEIRO
+
+
+        while(fscanf(ficheiro, "%d%c", &linhas_ficheiro[file], &colunas_ficheiro[file]) != EOF){ // LEITURA DO FICHEIRO
+
+            printf("%d%c\n", linhas_ficheiro[file], colunas_ficheiro[file]); ///PRINTA CONTEUDO DO FICHEIRO
+        }
+        if(!ficheiro){
+            printf("Ficheiro  %s não  encontrado", argv[1]);
+            return 1;
+        }
+
+        else if(argc > 2){
+            printf("Impossivel abrir  %s ficheiro", argv[1] );
+        }
+        fclose(ficheiro);
+    }
+    return 0;
 
 }
--- othello_func.h
+++ othello_func.h
@@ -9,7 +9,6 @@ void flanked(char board[SZ][SZ],int line, int col,char jogador);
 int jogadas_validas(char board[SZ][SZ], int linha, int col, char jogador);
 void play(char board[SZ][SZ], int line, int col, char player);
 void temp_board(char board[SZ][SZ], char new_board[SZ][SZ]);
-int not_arrayMoves(int line, int col, int moves[][SZ]);
 int pontos(char player,char board[SZ][SZ]);
 void fim_jogo(char board[SZ][SZ], char jogador);
-void jogada_pc(char board[SZ][SZ], int moves[][SZ], char color);
+void input_output( char board[SZ][SZ],int turn);
